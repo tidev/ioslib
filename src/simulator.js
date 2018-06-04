1dbec8a334fe2914ebdcc9c219b7f42203635f35
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-06-04 16:19:28 -0700

    Support Xcode 10, bump ioslib to 2.2.0

--- src/simulator.js
+++ src/simulator.js
@@ -19,35 +19,6 @@ import { expandPath } from 'appcd-path';
  * @type {Object}
  */
 export const devicePairCompatibility = {
-	'>=6.2 <7.0': {        // Xcode 6.2, 6.3, 6.4
-		'>=8.2 <9.0': {    // iOS 8.2, 8.3, 8.4
-			'1.x': true    // watchOS 1.0
-		}
-	},
-	'7.x': {               // Xcode 7.x
-		'>=8.2 <9.0': {    // iOS 8.2, 8.3, 8.4
-			'1.x': true    // watchOS 1.0
-		},
-		'>=9.0 <=9.2': {   // iOS 9.0, 9.1, 9.2
-			'>=2.0 <=2.1': true // watchOS 2.0, 2.1
-		},
-		'>=9.3': {         // iOS 9.x
-			'2.2': true    // watchOS 2.2
-		}
-	},
-	'8.x': {               // Xcode 8.x
-		'>=9.0 <=9.2': {   // iOS 9.0, 9.1, 9.2
-			'>=2.0 <=2.1': true // watchOS 2.0, 2.1
-		},
-		'>=9.3': {         // iOS 9.x
-			'2.2': true,   // watchOS 2.2
-			'3.x': true    // watchOS 3.x
-		},
-		'10.x': {          // iOS 10.x
-			'2.2': true,   // watchOS 2.2
-			'3.x': true    // watchOS 3.x
-		}
-	},
 	'9.x': {               // Xcode 9.x
 		'>=9.0 <=9.2': {   // iOS 9.0, 9.1, 9.2
 			'>=2.0 <=2.1': true // watchOS 2.0, 2.1
@@ -60,9 +31,27 @@ export const devicePairCompatibility = {
 			'2.2': true,   // watchOS 2.2
 			'3.x': true    // watchOS 3.x
 		},
-		'11.x': {
+		'11.x': {		   // iOS 11.x
 			'>=3.2': true, // watchOS 3.2
 			'4.x': true    // watchOS 4.x
+		}
+	},
+	'10.x': {              // Xcode 10.x
+		'>=9.3': {         // iOS 9.x
+			'2.2': true,   // watchOS 2.2
+			'3.x': true    // watchOS 3.x
+		},
+		'10.x': {          // iOS 10.x
+			'2.2': true,   // watchOS 2.2
+			'3.x': true    // watchOS 3.x
+		},
+		'11.x': {		   // iOS 11.x
+			'>=3.2': true, // watchOS 3.2
+			'4.x': true    // watchOS 4.x
+		},
+		'12.x': {		   // iOS 12.x
+			'>=4.x': true, // watchOS 4.x
+			'5.x': true    // watchOS 5.x
 		}
 	}
 };
